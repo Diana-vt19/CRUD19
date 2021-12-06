6751a35bab6d26aa6791f0d3bf19a729b14ac1e7
Diana-vt19 <dzemilevadiana@gmail.com>
2021-12-06 16:45:14 +0200

    Add services and javadoc

---
+++ src/main/java/edu/javavt19/service/CarBrandJdbcServiceImpl.java
@@ -0,0 +1,35 @@
+package edu.javavt19.service;
+
+import edu.javavt19.dao.CarBrandDAO;
+import edu.javavt19.model.CarBrand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Класс CarBrand для Jdbc
+ */
+@Service("carBrandJdbcService")
+public class CarBrandJdbcServiceImpl implements CarBrandService {
+    @Autowired
+    @Qualifier("getCarBrandJdbcDAO")
+    private CarBrandDAO carBrandDAO;
+
+    public void saveOrUpdate(CarBrand item) {
+        carBrandDAO.saveOrUpdate(item);
+    }
+
+    public void delete(int itemId) {
+        carBrandDAO.delete(itemId);
+    }
+
+    public CarBrand get(int itemId) {
+        return carBrandDAO.get(itemId);
+    }
+
+    public List<CarBrand> list() {
+        return carBrandDAO.list();
+    }
+}
